da7586ef636389f2918a8e497fa4795ef3f4dea2
Animesh Kumar <animesh.ak19@gmail.com>
2023-07-29 23:34:21 +0530

    Update 7_Triangle_or_not.py
    Can form triangle or not

--- Python_Problems/7_Triangle_or_not.py
+++ Python_Problems/7_Triangle_or_not.py
@@ -4,4 +4,4 @@ s3=float(input('Enter side 3 Length'))
 if s1+s2>s3 and s2+s3>s1 and s1+s3>s2:
     print('it is a triangle')
 else:
-    print('it is not a triangle')
+    print('it is not a triangle') 
