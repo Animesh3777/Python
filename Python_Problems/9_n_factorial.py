20de1dded9c4bdc369ea04adc42eb270eb8b6ca7
Animesh Kumar <animesh.ak19@gmail.com>
2023-07-31 02:01:20 +0530

    Update 9_n_factorial.py

--- Python_Problems/9_n_factorial.py
+++ Python_Problems/9_n_factorial.py
@@ -1,5 +1,11 @@
-n = int(input("Enter the number"))
-fact = 1
-for i in range(1, n + 1):
-    fact *= i
-print(fact)
+# n = int(input("Enter the number"))
+# fact = 1
+# for i in range(1, n + 1):
+#     fact *= i
+# print(fact)
+
+# i=1
+# while i<=n:
+#     fact*=i
+#     i+=1
+# print(fact)
