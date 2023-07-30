e7b88be42f11477780120535ba52141c4e6dc385
Animesh Kumar <animesh.ak19@gmail.com>
2023-07-31 02:01:29 +0530

    Create 12_Circumfrence_of_circle.py

---
+++ Python_Problems/12_Circumfrence_of_circle.py
@@ -0,0 +1,5 @@
+radius = float(input("Enter radius of the circle"))
+pie_value = 3.14
+# circumfrence of circle is 2*pie*radius
+coc = 2 * pie_value * radius
+print("Circumfrence of the circle is :", coc)
