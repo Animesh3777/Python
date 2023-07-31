265382790e6a102d828f435f61c995477f432469
Animesh Kumar <animesh.ak19@gmail.com>
2023-07-31 23:17:47 +0530

    Update 14_Sum_of_N.py

--- Python_Problems/14_Sum_of_N.py
+++ Python_Problems/14_Sum_of_N.py
@@ -1,6 +1,32 @@
 n=int(input('enter the number'))
 sum=0
-for i in range (1,n+1):
-    print(sum+i)
-    sum+=i
-print('sum of',n,'number is',sum)
+#for loop
+# for i in range (1,n+1):
+    # print(sum+i)
+#     sum+=i
+# print('sum of first ',"n",'number is',sum)
+
+# n = int(input("Enter the number"))
+
+#while loop
+# i = 0
+# while i <= n:
+#     sum = sum + i
+#     i = i + 1
+# print(sum)
+
+
+# for i in range(1, n + 1):
+#     num = int(input("enter the number"))
+#     sum += num
+#     i += 1
+#     print(i)
+# print(sum)
+
+
+# i = 1
+# while i <= n:
+#     num = int(input("Enter the number"))
+#     sum += num
+#     i =i+1
+# print(sum)
