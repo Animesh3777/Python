bb430ac063e00d8b61732eb6dff512813440b569
Animesh Kumar <animesh.ak19@gmail.com>
2023-08-04 23:50:50 +0530

    Create 3_Area_Perimeter_of_Square.py

---
+++ Python_Basic/3_Area_Perimeter_of_Square.py
@@ -0,0 +1,12 @@
+side = int(input("Enter One Side Value of the Square"))
+Area_of_square = side * side
+Perimeter_of_square = 4 * side
+print(
+    side,
+    ",",
+    "Area of Square is :",
+    Area_of_square,
+    "&",
+    "Perimeter of Square is ",
+    Perimeter_of_square,
+)
