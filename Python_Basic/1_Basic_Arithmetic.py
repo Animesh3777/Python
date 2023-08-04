e52f4bcc1688ba2b70938cd1da7e2acbad0d545c
Animesh Kumar <animesh.ak19@gmail.com>
2023-08-04 23:50:31 +0530

    Create 1_Basic_Arithmetic.py

---
+++ Python_Basic/1_Basic_Arithmetic.py
@@ -0,0 +1,11 @@
+a = int(input("write your 1st number"))
+b = int(input("write your 2nd number"))
+add = a + b
+sub = a - b
+multiply = a * b
+division = a / b
+
+print("addition of your two digit number is", add)
+print("subtraction of your two digit number is", sub)
+print("multiplication of your two digit number is", multiply)
+print("division of your two digit number is", division)
