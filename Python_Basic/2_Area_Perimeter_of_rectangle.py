b22a4d45132761771f614ae477557434e9218d98
Animesh Kumar <animesh.ak19@gmail.com>
2023-08-04 23:50:48 +0530

    Create 2_Area_Perimeter_of_rectangle.py

---
+++ Python_Basic/2_Area_Perimeter_of_rectangle.py
@@ -0,0 +1,9 @@
+length = int(input("Whats the length of the rectangle?"))
+breadth = int(input("Whats the breadth of the rectangle?"))
+print(length, breadth)
+print(
+    "Area of rectangle is :",
+    length * breadth,
+    "& Perimeter of rectangle is :",
+    2 * (length + breadth),
+)
