b7dc9c9062cb20a833e2379004deb2ec3e49e135
Animesh Kumar <animesh.ak19@gmail.com>
2023-08-04 23:50:55 +0530

    Create 5_Type_Conersion.py

---
+++ Python_Basic/5_Type_Conersion.py
@@ -0,0 +1,16 @@
+x = input()
+# print(x, "Hello")
+
+"""x_int = int(x)
+print(type(x_int))"""
+
+"""y = str(x)
+print(y, type(y))
+
+print(x,type(x))"""
+
+# y=bool(x)
+# print(y,type(y))
+
+# print(bool("")," why?")
+# x = int(input())
