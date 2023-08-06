22c5e538be94526d29b39a8aa36f07068b5961b1
Animesh Kumar <animesh.ak19@gmail.com>
2023-08-06 16:09:52 +0530

    Create 7_Swap_2_Variables.py

---
+++ Python_Basic/7_Swap_2_Variables.py
@@ -0,0 +1,17 @@
+# x=100
+# y=200
+# # temp=x
+# # x=y
+# # y=temp
+
+# # print(x,y)
+# # print(temp)
+# x,y=y,x
+# print(x,y)
+# print(y,x)
+
+x,y,z=100,"XYZ",10.5
+print(x,y,z)
+x,y=x-5,y+"ABC"
+print(x)
+print(y)
